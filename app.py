adbc9b0a4d63d5e10b29e65952b7508bce219641
GuyAbramov <93131205+GuyAbramov@users.noreply.github.com>
2021-12-11 20:41:56 +0200

    push

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask
-
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/home')
+def home():
+    return 'home page'
+
+
+@app.route('/intro')
+def intro():
+    return 'intro page'
+
+@app.route('/intro2')
+def intro2():
+    return 'intro page123'
+
+@app.route('/wrong')
+def wrong():
+    return 'oh no page not found!'
+
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
